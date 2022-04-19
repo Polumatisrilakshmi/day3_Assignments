e08bbe98fb6cc81d1e05de031687c1f413369a9b
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-04-19 11:20:33 +0530

    Added: Wages and hours

---
+++ EmployeeWageHours.java
@@ -0,0 +1,20 @@
+public class EmployeeWageHours {
+
+    public static void main(String[] args) {
+        // constants
+        int IS_FULL-TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        // variables
+        int empHrs = 0;
+        int empWage = 0;
+        // computation
+        double empCheck = Math.floor(math.random() * 10) % 2;
+        if (empcheck == IS_FULL-TIME )
+            empHrs = 8;
+        empWage = emoHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage : " + empWage);
+
+
+    }
+
+}
